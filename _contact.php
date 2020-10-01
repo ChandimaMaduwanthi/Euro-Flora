ebcc2b172149c3e9fd12de733a24e2d9304b4988
ChandimaMaduwanthi <48376228+ChandimaMaduwanthi@users.noreply.github.com>
2020-10-01 17:28:00 +0530

    File upload finished

--- _contact.php
+++ _contact.php
@@ -1,11 +1,37 @@
 <?php
 
+include 'db.php';
+
 $name = $_REQUEST['names'];
 $email = $_REQUEST['emails'];
 $phone = $_REQUEST['phone'];
 $ordercode = $_REQUEST['code'];
 $message = $_REQUEST['message'];
 
-echo $name . $email . $phone . $ordercode . $message;
+$file = $_FILES['file'];
+$filename = $_FILES['file']['name'];
+$filetempname = $_FILES['file']['tmp_name'];
+$filetype = $_FILES['file']['type'];
+$fileerr = $_FILES['file']['error'];
+$filesize = $_FILES['file']['size'];
+
+$extension = array('jpg','png','jpeg');
+$fileext = strtolower(end(explode(".", $filename)));
+$attachment = uniqid('',true).".".$fileext;
+
+try{
+    $sql = "INSERT INTO `contact`(`name`, `email`, `phone`, `order_code`, `message`, `attachment`) VALUES ('".$name."','".$email."','".$phone."','".$ordercode."','".$message."','".$attachment."')";
+    $stmt= $conn->prepare($sql);
+    $stmt->execute();
+
+    if($stmt->rowCount()==1){
+        $filepath = "files/".$attachment;
+        move_uploaded_file($filetempname,$filepath);
+        echo "File uploaded";
+    }
+
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
 
 ?>
